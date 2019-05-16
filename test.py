6a27e34a1e079b9f4917b29c9084e94e3299d9f4
root <root@LAPTOP-KMFP2CKR.localdomain>
2019-05-16 12:50:34 -0400

    First upload of test.py

---
+++ test.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May 12 10:18:39 2019
+
+@author: neilh
+"""
+
+i=0
+p=0
+for i in range(0,36):
+   p+=i
+print(p)
+k=0
+def iseven(k):
+   return k%2==0
+
+print iseven(7)
